26b638c4d7b75582a13fc3bcb86f0099b7e117a8
niltonjuniorfoz <niltonjuniorfoz@gmail.com>
2026-09-02 23:06:45 -0300

    fix: carregar traducoes da loja no pedido

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './storefront-fixes.css';
+import './pages/store/i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
